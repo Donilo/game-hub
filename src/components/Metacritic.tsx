e626cb782eed68204b7b46245e289adc11d36104
Emran Miri <11910359+Donilo@users.noreply.github.com>
2023-09-24 16:39:58 +0330

    Displaying metacritics

---
+++ src/components/Metacritic.tsx
@@ -0,0 +1,11 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  metacritic: number;
+}
+
+export default function Metacritic({ metacritic }: Props) {
+  const color = metacritic > 80 ? "green" : metacritic > 60 ? "yellow" : "";
+
+  return <Badge colorScheme={color}>{metacritic}</Badge>;
+}
