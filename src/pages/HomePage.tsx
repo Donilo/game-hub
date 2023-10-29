e0f8cc9c3316a3be217fe234b59c7ef019687df6
Emran Miri <11910359+Donilo@users.noreply.github.com>
2023-10-30 01:23:52 +0330

    Setup routing

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,42 @@
+import { Grid, GridItem, Show, HStack } from "@chakra-ui/react";
+import GameGrid from "../components/GameGrid";
+import GameHeading from "../components/GameHeading";
+import GenreList from "../components/GenreList";
+import PlatformSelector from "../components/PlatformSelector";
+import SortSelector from "../components/SortSelector";
+
+export default function HomePage() {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"main"`,
+        lg: `"aside main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
+    >
+      <Show above="lg">
+        <GridItem area="aside" paddingX={5}>
+          <GenreList />
+        </GridItem>
+      </Show>
+
+      <GridItem
+        area="main"
+        padding={{
+          base: 2,
+          lg: 5,
+        }}
+      >
+        <GameHeading />
+        <HStack marginY={3} spacing={3}>
+          <PlatformSelector />
+          <SortSelector />
+        </HStack>
+        <GameGrid />
+      </GridItem>
+    </Grid>
+  );
+}
